5cac741d2c921e79033ed3b851c6f923d64cdc99
Jacob Rein <jakerein@gmail.com>
2022-04-08 07:17:33 -0400

    Update AirBarCompose.kt
    - modified AirBar to more match other Composables, using state changes instead of keeping the value internally.
     - this allows us to set an initial value, change the value on the fly, and even be able to animate changes.
    - changed the icon to be a Composable lamba so the user/developer can pass their own icon-like Composable in, just like in other Composables
    - changed the default colors to be apart of the MaterialTheme colors
    - cornerRadius takes in a CornerRadius so the user/developer can do what they want with the corners
    
    I know this changes the Compose version from the XML version quite a bit, but the changes to the XML version shouldn't be that difficult to implement.

--- AirBar/src/main/java/com/shahryar/airbar/AirBarCompose.kt
+++ AirBar/src/main/java/com/shahryar/airbar/AirBarCompose.kt
@@ -1,10 +1,10 @@
 package com.shahryar.airbar
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -13,102 +13,122 @@ import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.RoundRect
 import androidx.compose.ui.graphics.*
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.input.pointer.pointerInteropFilter
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+
+class AirBarController(
+    progress: Double = 0.0,
+    progressCoordinates: Float = 0f,
+    internal val isHorizontal: Boolean = false,
+    animateProgress: Boolean = true
+) {
+
+    var animateProgress by mutableStateOf(animateProgress)
+    internal var internalProgress by mutableStateOf(progress)
+    internal var progressCoordinates by mutableStateOf(progressCoordinates)
+
+    var progress: Double
+        get() = internalProgress
+        set(value) {
+            progressCoordinates = reverseCalculateValues(value).toFloat()
+            internalProgress = value
+        }
+
+    internal var bottomY = 0f
+    internal var rightX = 0f
+
+    private fun reverseCalculateValues(realPercentage: Double): Double {
+        val p = if (isHorizontal)
+            realPercentage * rightX / 100
+        else
+            bottomY - realPercentage * bottomY / 100
+
+        return String.format("%.2f", p).toDouble()
+    }
+
+}
+
+@Composable
+fun rememberAirBarController(
+    progress: Double = 0.0,
+    progressCoordinates: Float = 0f,
+    isHorizontal: Boolean = false,
+    animateProgress: Boolean = true
+) = remember { AirBarController(progress, progressCoordinates, isHorizontal, animateProgress) }
 
 @ExperimentalComposeUiApi
 @Composable
 fun AirBar(
     modifier: Modifier = Modifier,
-    fillColor: Color = colorResource(id = R.color.defaultLevel),
+    controller: AirBarController = rememberAirBarController(),
+    fillColor: Color = MaterialTheme.colors.primary,
     fillColorGradient: List<Color>? = null,
-    backgroundColor: Color = colorResource(id = R.color.defaultBackground),
-    cornerRadius: Dp = 40.dp,
+    backgroundColor: Color = MaterialTheme.colors.background,
+    cornerRadius: CornerRadius = CornerRadius(x = 40.dp.value, y = 40.dp.value),
     minValue: Double = 0.0,
     maxValue: Double = 100.0,
-    icon: Painter? = null,
-    isHorizontal: Boolean = false,
-    onValuesChanged: (percentage: Double, value: Double) -> Unit
+    icon: (@Composable () -> Unit)? = null,
+    valueChanged: (Double) -> Unit
 ) {
-    var progress: Float by remember {
-        mutableStateOf(0F)
-    }
 
-    var bottomY = 0f
-    var rightX = 0f
-
-    /**
-     * Calculate progress
-     */
-    fun calculateValues(touchY: Float, touchX: Float): Pair<Double, Double> {
-        val rawPercentage = if (isHorizontal) {
-            String.format("%.2f", (touchX.toDouble() / rightX.toDouble()) * 100).toDouble()
+    fun calculateValues(touchY: Float, touchX: Float): Double {
+        val rawPercentage = if (controller.isHorizontal) {
+            String.format("%.2f", (touchX.toDouble() / controller.rightX.toDouble()) * 100).toDouble()
         } else {
-            String.format("%.2f", 100 - ((touchY.toDouble() / bottomY.toDouble()) * 100)).toDouble()
+            String.format("%.2f", 100 - ((touchY.toDouble() / controller.bottomY.toDouble()) * 100)).toDouble()
         }
-        val percentage =
-            if (rawPercentage < 0) 0.0 else if (rawPercentage > 100) 100.0 else rawPercentage
 
-        val value = String.format("%.2f", ((percentage / 100) * (maxValue - minValue) + minValue))
-            .toDouble()
+        val percentage = if (rawPercentage < 0) 0.0 else if (rawPercentage > 100) 100.0 else rawPercentage
 
-        return Pair(percentage, value)
+        return String.format("%.2f", ((percentage / 100) * (maxValue - minValue) + minValue)).toDouble()
     }
 
-    Box(modifier = modifier, contentAlignment = if (isHorizontal) Alignment.CenterStart else Alignment.BottomCenter) {
+    val vertical = if (controller.isHorizontal) 0f else controller.progressCoordinates
+    val horizontal = if (controller.isHorizontal) controller.progressCoordinates else controller.rightX
+
+    val bottomToTop = if(controller.animateProgress) animateFloatAsState(vertical).value else vertical
+    val startToEnd = if(controller.animateProgress) animateFloatAsState(horizontal).value else horizontal
+
+    Box(modifier = modifier, contentAlignment = if (controller.isHorizontal) Alignment.CenterStart else Alignment.BottomCenter) {
         Canvas(modifier = modifier.pointerInteropFilter { event ->
-            if (!isHorizontal) {
+            if (!controller.isHorizontal) {
                 when {
-                    event.y in 0.0..bottomY.toDouble() -> {
-                        progress = event.y
-                        val values = calculateValues(event.y, event.x)
-                        onValuesChanged(values.first, values.second)
+                    event.y in 0.0..controller.bottomY.toDouble() -> {
+                        valueChanged(calculateValues(event.y, event.x))
                         true
                     }
                     event.y > 100 -> {
-                        progress = bottomY
-                        val values = calculateValues(event.y, event.x)
-                        onValuesChanged(values.first, values.second)
+                        valueChanged(calculateValues(event.y, event.x))
                         true
                     }
                     event.y < 0 -> {
-                        progress = 0F
-                        val values = calculateValues(event.y, event.x)
-                        onValuesChanged(values.first, values.second)
+                        valueChanged(calculateValues(event.y, event.x))
                         true
                     }
                     else -> false
                 }
             } else {
                 when {
-                    event.x in 0.0..rightX.toDouble() -> {
-                        progress = event.x
-                        val values = calculateValues(event.y, event.x)
-                        onValuesChanged(values.first, values.second)
+                    event.x in 0.0..controller.rightX.toDouble() -> {
+                        valueChanged(calculateValues(event.y, event.x))
                         true
                     }
                     event.x > 100 -> {
-                        progress = rightX
-                        val values = calculateValues(event.y, event.x)
-                        onValuesChanged(values.first, values.second)
+                        valueChanged(calculateValues(event.y, event.x))
                         true
                     }
                     event.x < 0 -> {
-                        progress = 0F
-                        val values = calculateValues(event.y, event.x)
-                        onValuesChanged(values.first, values.second)
+                        valueChanged(calculateValues(event.y, event.x))
                         true
                     }
                     else -> false
                 }
             }
         }) {
-            bottomY = size.height
-            rightX = size.width
+            controller.bottomY = size.height
+            controller.rightX = size.width
+            if(controller.internalProgress > 0.0) controller.progress = controller.internalProgress
 
             val path = Path()
             path.addRoundRect(
@@ -117,7 +137,7 @@ fun AirBar(
                     0F,
                     size.width,
                     size.height,
-                    CornerRadius(x = cornerRadius.value, y = cornerRadius.value)
+                    cornerRadius
                 )
             )
             drawContext.canvas.drawPath(path, Paint().apply {
@@ -125,26 +145,25 @@ fun AirBar(
                 isAntiAlias = true
             })
             drawContext.canvas.clipPath(path = path, ClipOp.Intersect)
-            drawContext.canvas.drawRect(0F, if (isHorizontal) 0f else progress, if (isHorizontal) progress else size.width, size.height, Paint().apply {
-                color = fillColor
-                isAntiAlias = true
-                if (!fillColorGradient.isNullOrEmpty() && fillColorGradient.size > 1) {
-                    shader = LinearGradientShader(
-                        from = Offset(0f, 0f),
-                        to = Offset(size.width, size.height),
-                        colors = fillColorGradient
-                    )
-                }
-            })
+            drawContext.canvas.drawRect(
+                0F,
+                bottomToTop,
+                startToEnd,
+                size.height,
+                Paint().apply {
+                    color = fillColor
+                    isAntiAlias = true
+                    if (!fillColorGradient.isNullOrEmpty() && fillColorGradient.size > 1) {
+                        shader = LinearGradientShader(
+                            from = Offset(0f, 0f),
+                            to = Offset(size.width, size.height),
+                            colors = fillColorGradient
+                        )
+                    }
+                })
         }
 
-        icon?.let {
-            Icon(
-                modifier = if (!isHorizontal) Modifier.padding(bottom = 15.dp) else Modifier.padding(start = 15.dp),
-                painter = it,
-                contentDescription = ""
-            )
-        }
+        icon?.let { Box(modifier = if (!controller.isHorizontal) Modifier.padding(bottom = 15.dp) else Modifier.padding(start = 15.dp)) { it() } }
     }
 }
 
@@ -152,5 +171,6 @@ fun AirBar(
 @Composable
 @Preview
 fun Preview() {
-    AirBar { _, _ -> }
+    val airBar = rememberAirBarController(45.0)
+    AirBar { airBar.progress = it }
 }
